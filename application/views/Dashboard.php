7cb42767f9d1c1ae6479fb95658629b4f8f0042f
adji142 <prasetyoajiw@gmail.com>
2019-09-18 00:18:50 +0700

    finish all - db - demo mode

--- application/views/Dashboard.php
+++ application/views/Dashboard.php
@@ -3,6 +3,25 @@
     require_once(APPPATH."views/parts/sidebar.php");
     $active = 'dashboard';
 ?>
+<style type="text/css">
+  #background{
+    position:absolute;
+    z-index:0;
+    background:white;
+    display:block;
+    min-height:100%; 
+    min-width:100%;
+    color:yellow;
+}
+
+#bg-text
+{
+    color:lightgrey;
+    font-size:60px;
+    transform:rotate(300deg);
+    -webkit-transform:rotate(300deg);
+}
+</style>
 <!--main-container-part-->
 <div id="content">
 <!--breadcrumbs-->
@@ -12,7 +31,10 @@
 <!--End-breadcrumbs-->
 
     <div class="row-fluid">
-      
+      <div id="background">
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <center><p id="bg-text">Demo Mode <br><br><br> <a id="bg-text" href="aistrick.com">aistrick.com</a></p></center>
+  </div>
     </div>
   </div>
 </div>
