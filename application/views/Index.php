257d57d907867e804ddc3b8c27653f2b99fb8911
adji142 <prasetyoajiw@gmail.com>
2019-09-23 21:14:40 +0700

    finish all - rev 1 - 01

--- application/views/Index.php
+++ application/views/Index.php
@@ -22,7 +22,7 @@
     <body>
         <div id="loginbox">            
             <form id="loginform" class="form-vertical" action="index.html">
-				 <div class="control-group normal_text"> <h3><img src="<?php echo base_url();?>Assets/img/logo.png" alt="Logo" /></h3></div>
+				 <div class="control-group normal_text"> <h3><img src="<?php echo base_url();?>Assets/img/logo_2.png" alt="Logo" /></h3></div>
                 <div class="control-group">
                     <div class="controls">
                         <div class="main_input_box">
